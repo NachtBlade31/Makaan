eca968aaac2b8610d32d8f6d257a9677dae8ee74
KSHITIZ BHARGAV <kbgoku@gmail.com>
2023-02-26 21:06:44 +0530

    third commit

--- makaan/settings.py
+++ makaan/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'makaan/static')
 ]
+
+# Media Folder Setting
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
